f7e84a42fe7c1deb12d9639db0358f8d9d2e63dc
Boris Shek <boris.shek@xitaso.com>
2026-09-09 18:56:40 +0200

    refactor

--- target/debug/deps/crate_a-3f74ff068d3dbb15.d
+++
@@ -1,5 +0,0 @@
-/home/borisshek/projects/poc-clippy-shared-lints/target/debug/deps/crate_a-3f74ff068d3dbb15.d: crate-a/src/main.rs
-
-/home/borisshek/projects/poc-clippy-shared-lints/target/debug/deps/libcrate_a-3f74ff068d3dbb15.rmeta: crate-a/src/main.rs
-
-crate-a/src/main.rs:
--- target/debug/deps/crate_b-bd6e0f1400c71585.d
+++
@@ -1,5 +0,0 @@
-/home/borisshek/projects/poc-clippy-shared-lints/target/debug/deps/crate_b-bd6e0f1400c71585.d: crate-b/src/main.rs
-
-/home/borisshek/projects/poc-clippy-shared-lints/target/debug/deps/libcrate_b-bd6e0f1400c71585.rmeta: crate-b/src/main.rs
-
-crate-b/src/main.rs:
